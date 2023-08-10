3c0d3c88251dae70a16fd027e568cfc38222b787
David Mees Knijnik <kdavidk98@gmail.com>
2023-08-10 19:05:23 -0300

    Added Hat 1 animations and equipping

--- Assets/Scripts/CharacterAnimator.cs
+++ Assets/Scripts/CharacterAnimator.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class CharacterAnimator : MonoBehaviour
 {
-    public Animator animator;
+    public Animator[] animators;
+    public RuntimeAnimatorController[] hatAnimations;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            animators[1].runtimeAnimatorController = hatAnimations[0];
+
+            foreach (Animator animator in animators)
+            {
+                animator.Play("Idle Down");
+            }
+        }
+    }
 
     public void SetMovementAnimation(Vector2 direction)
     {
-      
-        animator.SetBool("Walking Up", direction.y > 0);
-        animator.SetBool("Walking Down", direction.y < 0);
-        animator.SetBool("Walking Right", direction.x > 0);
-        animator.SetBool("Walking Left", direction.x < 0);
+        foreach (Animator animator in animators)
+        {
+            if (animator.runtimeAnimatorController != null)
+            {
+                animator.SetBool("Walking Up", direction.y > 0);
+                animator.SetBool("Walking Down", direction.y < 0);
+                animator.SetBool("Walking Right", direction.x > 0);
+                animator.SetBool("Walking Left", direction.x < 0);
+            }
+        }
 
     }
 }
