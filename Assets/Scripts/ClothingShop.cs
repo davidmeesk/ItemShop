f412b2894124de923227015f35c8e55f85a1294e
David Mees Knijnik <kdavidk98@gmail.com>
2023-08-11 15:14:53 -0300

    clothing icons and information sent to UI

--- Assets/Scripts/ClothingShop.cs
+++ Assets/Scripts/ClothingShop.cs
@@ -2,11 +2,32 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ClothingShop : MonoBehaviour
 {
+    [System.Serializable]
+    public struct ClothingData
+    {
+        public Sprite icon;
+        public string clothingName;
+        public float price;
+        public bool purchased;
+    }
+
     [SerializeField]
     private GameObject shopPanel;
+    [SerializeField]
+    private ClothingData[] shopData;
+
+    public delegate void UpdateUI(ClothingData[] clothingData);
+    public event UpdateUI updateShopUI;
+
+    private void Start()
+    {
+        updateShopUI += shopPanel.GetComponent<ClothingShopUI>().UpdateUI;
+        updateShopUI.Invoke(shopData);
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
--- Assets/Scripts/ClothingShopUI.cs
+++ Assets/Scripts/ClothingShopUI.cs
@@ -1,18 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System;
 
 public class ClothingShopUI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [System.Serializable]
+    public struct ClothingInfo
     {
-        
+        public Image icon;
+        public TMP_Text clothingName;
+        public TMP_Text price;
+        public bool purchased;
     }
+    public ClothingInfo[] clothing;
 
-    // Update is called once per frame
-    void Update()
+    internal void UpdateUI(ClothingShop.ClothingData[] clothingData)
     {
-        
+        for(int clothingIndex = 0; clothingIndex < clothingData.Length; clothingIndex++)
+        {
+            clothing[clothingIndex].icon.sprite = clothingData[clothingIndex].icon;
+            clothing[clothingIndex].clothingName.text = clothingData[clothingIndex].clothingName;
+            clothing[clothingIndex].price.text = clothingData[clothingIndex].price.ToString();
+            clothing[clothingIndex].purchased = clothingData[clothingIndex].purchased;
+        }
     }
 }
